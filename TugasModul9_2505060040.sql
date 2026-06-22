ba5c751c92e00a5fbab3e73fc24cde9d7c1ef97d
N4uf4l11 <sorgama1007@gmail.com>
2026-06-23 01:55:14 +0700

    Add files via upload

---
+++ TugasModul9_2505060040.sql
@@ -0,0 +1,251 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 22, 2026 at 06:54 PM
+-- Server version: 8.4.3
+-- PHP Version: 8.3.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toko_buku`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `tambah_transaksi` (`p_id_pelanggan` INT, `p_id_buku` INT, `p_jumlah` INT)   BEGIN
+    DECLARE v_harga DECIMAL(10,2);
+    DECLARE v_stok  INT;
+    DECLARE v_total DECIMAL(10,2);
+
+    -- Ambil harga dan stok buku
+    SELECT harga, stok INTO v_harga, v_stok
+    FROM buku
+    WHERE id_buku = p_id_buku;
+
+    -- Validasi stok
+    IF v_stok < p_jumlah THEN
+        SELECT 'Error: Stok buku tidak mencukupi!' AS pesan;
+    ELSE
+        -- Hitung total harga
+        SET v_total = v_harga * p_jumlah;
+
+        -- Kurangi stok buku
+        UPDATE buku
+        SET stok = stok - p_jumlah
+        WHERE id_buku = p_id_buku;
+
+        -- Insert ke tabel transaksi
+        INSERT INTO transaksi (id_pelanggan, id_buku, jumlah, total_harga, tanggal_transaksi)
+        VALUES (p_id_pelanggan, p_id_buku, p_jumlah, v_total, CURDATE());
+
+        -- Update total belanja pelanggan
+        UPDATE pelanggan
+        SET total_belanja = total_belanja + v_total
+        WHERE id_pelanggan = p_id_pelanggan;
+
+        SELECT 'Transaksi berhasil' AS pesan;
+    END IF;
+END$$
+
+--
+-- Functions
+--
+CREATE DEFINER=`root`@`localhost` FUNCTION `hitung_diskon` (`total_belanja` DECIMAL(10,2)) RETURNS DECIMAL(5,2) DETERMINISTIC BEGIN
+    DECLARE diskon DECIMAL(5,2);
+
+    IF total_belanja >= 5000000 THEN
+        SET diskon = 10.00;
+    ELSEIF total_belanja >= 1000000 THEN
+        SET diskon = 5.00;
+    ELSE
+        SET diskon = 0.00;
+    END IF;
+
+    RETURN diskon;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `penulis` varchar(100) NOT NULL,
+  `harga` decimal(10,2) NOT NULL,
+  `stok` int DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `judul`, `penulis`, `harga`, `stok`) VALUES
+(1, 'Laut Bercerita', 'Leila S. Chudori', 50000.00, 972),
+(2, 'Harry Potter ', 'J.K. Rowling', 35000.00, 170),
+(3, 'Bumi', 'Tereliye', 30000.00, 180),
+(4, 'Negeri 5 Menara', 'Ahmad Fuadi', 55000.00, 850);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` int NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `total_belanja` decimal(10,2) DEFAULT '0.00',
+  `status_member` enum('REGULER','GOLD','PLATINUM') NOT NULL DEFAULT 'REGULER'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`id_pelanggan`, `nama`, `total_belanja`, `status_member`) VALUES
+(1, 'Susi Susanti', 800000.00, 'REGULER'),
+(2, 'Priyambodo Agung', 2500000.00, 'GOLD'),
+(3, 'Pria Perkoso', 12000000.00, 'PLATINUM'),
+(4, 'Dwi Jayasuardi', 0.00, 'REGULER');
+
+--
+-- Triggers `pelanggan`
+--
+DELIMITER $$
+CREATE TRIGGER `update_status_member` BEFORE UPDATE ON `pelanggan` FOR EACH ROW BEGIN
+    IF NEW.total_belanja >= 5000000 THEN
+        SET NEW.status_member = 'PLATINUM';
+    ELSEIF NEW.total_belanja >= 1000000 THEN
+        SET NEW.status_member = 'GOLD';
+    ELSE
+        SET NEW.status_member = 'REGULER';
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int NOT NULL,
+  `id_pelanggan` int NOT NULL,
+  `id_buku` int NOT NULL,
+  `jumlah` int NOT NULL,
+  `total_harga` decimal(10,2) NOT NULL DEFAULT (0),
+  `tanggal_transaksi` date NOT NULL DEFAULT (0)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_pelanggan`, `id_buku`, `jumlah`, `total_harga`, `tanggal_transaksi`) VALUES
+(1, 1, 2, 4, 140000.00, '2026-04-26'),
+(2, 3, 3, 10, 300000.00, '2026-05-23'),
+(3, 3, 4, 70, 3850000.00, '2026-03-22'),
+(4, 2, 1, 200, 10000000.00, '2026-02-20'),
+(5, 4, 1, 420, 21000000.00, '2026-04-27'),
+(6, 1, 1, 3, 150000.00, '2026-06-20'),
+(7, 2, 3, 5, 150000.00, '2026-06-20'),
+(8, 3, 4, 100, 5500000.00, '2026-06-20');
+
+--
+-- Triggers `transaksi`
+--
+DELIMITER $$
+CREATE TRIGGER `trg_hitung_total_harga` BEFORE INSERT ON `transaksi` FOR EACH ROW BEGIN
+    DECLARE harga_buku DECIMAL(10,2);
+
+    SELECT harga
+    INTO harga_buku
+    FROM buku
+    WHERE id_buku = NEW.id_buku;
+
+    SET NEW.total_harga = harga_buku * NEW.jumlah;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `FK__pelanggan` (`id_pelanggan`),
+  ADD KEY `FK__buku` (`id_buku`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `id_pelanggan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `FK__buku` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`),
+  ADD CONSTRAINT `FK__pelanggan` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
